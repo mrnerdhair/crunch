b50fdbe20a2e46782f692a4f632e35ce3c4e9ec9
Reid Rankin <reidrankin@gmail.com>
2024-03-02 00:59:59 -0700

    it runs

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -2,17 +2,15 @@
 
 use risc0_zkvm::guest::env;
 
-
-
 fn main() {
     // TODO: Implement your guest code here
 
     // read the input
-    let input: u32 = env::read();
+    // let input: u32 = env::read();
 
     // TODO: do something with the input
     crunch::fake_main();
 
     // write public output to the journal
-    env::commit(&input);
+    // env::commit(&input);
 }
